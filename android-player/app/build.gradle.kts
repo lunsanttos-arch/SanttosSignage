80b25ea9315e056a44b6d0ada4bf3e753caa4229
lunsanttos-arch <lunsanttos@gmail.com>
2026-09-15 01:50:31 -0300

    Set Android player version 0.1.5

--- android-player/app/build.gradle.kts
+++ android-player/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.santtos.signage"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.2"
+        versionCode = 5
+        versionName = "0.1.5"
     }
 
     compileOptions {

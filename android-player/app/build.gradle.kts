0b9670e8dee126c722a8fb2e093ee42cbd115a4a
lunsanttos-arch <lunsanttos@gmail.com>
2026-09-15 00:19:25 -0300

    Add Android app Gradle config

---
+++ android-player/app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+    namespace = "com.santtos.signage"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.santtos.signage"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.1.2"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.media3:media3-exoplayer:1.6.1")
+    implementation("androidx.media3:media3-ui:1.6.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+}
